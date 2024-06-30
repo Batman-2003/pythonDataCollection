96626fa02f84b8e9f2b7feb25a36c12eff441c33
Batman-2003 <g4ur4v03@gmail.com>
2024-06-30 19:02:48 +0530

    FUNCTION: First Commit USER: GK

--- serialTest.py
+++ serialTest.py
@@ -8,11 +8,39 @@
 # TODO: 5. Make if Pretty
 
 import numpy as np 
-import matplotlib.pylot as plt
+import matplotlib.pyplot as plt
 import serial
+import csv
+
+# Global Variables Sit Here
+PORT = "COM4"
+BAUDERATE = 115200
+LLIMIT = -3
+ULIMIT =  3
+X = np.arange(-3, 3+0.1, 0.1)
+Y = np.arange(-3, 3+0.1, 0.1)
+Z = np.arange(-3, 3+0.1, 0.1)
+points = np.zeros((1, np.size(X), 3), np.float64)
+
 
 def main():
-  pass
+  print("\n")
+  print("Starting the IMU-Calibration CLI Program")
+  print("Written by Gaurav Khaire in Last Weekend of June-2024 (or so he says)")
+  print("")
+  print("------------------------------------------------------------")
+  print("")
+  print("Printing out the Constant Parameters inside the script: ")
+  print("\t PORT      = " + str(PORT)     )
+  print("\t BAUDERATE = " + str(BAUDERATE))
+
+  print("The Points are as follows: ")
+  print(points)
+
+  fig = plt.figure(figsize=(8, 6))
+  ax = plt.axes(projection='3d')
+  ax.scatter3D(points)
+  plt.show()
 
 # --------------------------------------------------------------
 main()
